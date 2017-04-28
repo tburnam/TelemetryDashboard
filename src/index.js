ad2a1c2d0d0bd700f84187ff660417df6531273c
Tyler <tyler@burnamtech.com>
2017-04-27 22:31:06 -0400

    Added routing support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import './style.scss';
 
-const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Welcome} />
+        <Route path="/about" component={About} />
+        <Route exact path="/test/:id" component={Test} />
+      </div>
+    </Router>
+  );
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('main'));
