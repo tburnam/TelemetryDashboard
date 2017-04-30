41c01686c50d77581faa4f74c8a282196dcec9fe
Tyler <tyler@burnamtech.com>
2017-04-30 18:02:20 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import Counter from './containers/counter';
+import Controls from './containers/controls';
+
+// import App from './components/app';
+
 import './style.scss';
 
-const App = (props) => {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Route exact path="/" component={Welcome} />
-        <Route path="/about" component={About} />
-        <Route exact path="/test/:id" component={Test} />
-      </div>
-    </Router>
-  );
-};
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
+));
 
 const Nav = (props) => {
   return (
@@ -33,10 +34,32 @@ const About = (props) => {
   return <div> All there is to know about me </div>;
 };
 const Welcome = (props) => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <Counter />
+      <Controls />
+    </div>
+  );
 };
 const Test = (props) => {
   return <div> ID: {props.match.params.id} </div>;
 };
 
-ReactDOM.render(<App />, document.getElementById('main'));
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Welcome} />
+        <Route path="/about" component={About} />
+        <Route exact path="/test/:id" component={Test} />
+      </div>
+    </Router>
+  );
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('main'));
